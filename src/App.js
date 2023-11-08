53a62895c5fa1240babf5286a8727139a9769577
morpho-tom <achuk233@gmail.com>
2023-11-08 17:54:07 +0530

    8/11/23 update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-
-import './App.css';
+import React from 'react';
 import Navbar from './components/Navbar';
 
 const App = () => {
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Logo from "../../assets/logo.png"
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return(
         <nav className="bg-blue ">
             <div className="flex item-center font-medium justify-around">
                 <div>
-                    <img src={Logo} alt="logo" className="md:cursor-pointer "/>
+                    <img src={Logo} alt="logo" className="md:cursor-pointer  h-20 w-auto"/>
                 </div>
                 <ul>
                     <li>
-                        <Link to={'/'}  className="py-7 px-3 inline-block">
+                        {/* <Link to="/"  className="py-7 px-3 inline-block">
                             Home
-                        </Link> 
+                        </Link>  */}
                     </li>
                 </ul>
 
