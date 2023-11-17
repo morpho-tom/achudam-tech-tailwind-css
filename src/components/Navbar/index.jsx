a3c4760f7dfbf64802f70e282243e74a0245af9b
morpho-tom <achuk233@gmail.com>
2023-11-17 13:10:17 +0530

    humbergar menu complet

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Navlinks from './NavLink';
 import Logo from '../../assets/logo.png'
-import { Link } from 'react-router-dom';
 import SearchButton from './SearchButton'
 
 const Navbar = () => {
+  const [open, setopen] = useState(false)
   return (
     <>
       <nav className='bg-white '>
         <div className='flex item-center font-medium justify-around'>
-
-          <logo className='z-50 p-5 md:w-auto w-full' >  
-          <Link to='/'><img className='md:cursor-pointer p-1 ' src={Logo} alt='logo' /></Link>
-          </logo>
+          <div className='z-50 p-5 md:w-auto w-full flex justify-between' >
+            <Link to='/'><img className='md:cursor-pointer p-1 ' src={Logo} alt='logo' /></Link>
+            <div className='text-3xl' onClick={() => setopen(!open)}>
+              <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
+            </div>
+          </div>
 
           <ul className='md:flex hidden uppercase item-center gap-8 ' >
             <li>
@@ -31,17 +34,18 @@ const Navbar = () => {
               </Link>
             </li>
             <li className='py-7 px-3 inline-block hover:text-blue-400 '>
-            <Navlinks/>
+              <Navlinks />
             </li>
           </ul>
           <div className='pt-4 md:block hidden'>
-            <SearchButton/>
+            <SearchButton />
           </div>
 
           {/* mobile view navbar */}
 
-          <ul className={'md:hidden bg-white absolute w-full h-full buttom-0 py-24 pl-4'}>
-          <li>
+          <ul 
+         className={`md:hidden bg-white absolute w-full h-full bottom-0 py-24 pl-4 duration-500 ${open ? "left-0" : "hidden"}`}>
+            <li>
               <Link className='py-7 px-3 inline-block hover:text-blue-400' to="/">
                 Home
               </Link>
@@ -57,14 +61,14 @@ const Navbar = () => {
               </Link>
             </li>
             <li className='py-7 px-3 inline-block hover:text-blue-400 '>
-            <Navlinks/>
-            <div className='py-5'>
-              <SearchButton/>
-            </div>
+              <Navlinks />
+              <div className='py-5'>
+                <SearchButton />
+              </div>
             </li>
           </ul>
-        </div>
-      </nav>
+        </div >
+      </nav >
     </>
   );
 };
